822ab9944a7dfa7e44beb3c0172e9de943c7595e
joyeuxclement87 <tuyishimirejoyeuxclement@gmail.com>
2023-11-17 07:29:38 -0800

    working on argc and argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - th  entry point
+ * @argv: parameter contain array str for the name
+ * @argc: to be for printing name
+ * Return: the value to be returned
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
