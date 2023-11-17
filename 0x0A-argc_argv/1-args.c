a194eb6afdb68486c3ce13b6ba181ed7b3f36950
joyeuxclement87 <tuyishimirejoyeuxclement@gmail.com>
2023-11-17 07:39:49 -0800

    working on no2

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - the enrty point
+ * @argc: argumrnt number
+ * @argv: array argumrnt num
+ * Return: the value to returned
+ */
+
+int main(int argc, char *argv[]__attribute__((unused)))
+{
+	printf("%d\n", argc);
+	return (0);
+}
