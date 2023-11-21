88cf772826e3c906ebf24d2bfc1e41f334152187
joyeuxclement87 <tuyishimirejoyeuxclement@gmail.com>
2023-11-21 08:02:43 -0800

    working on string dupp

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _strdup - duplication of new mem alloc
+ * @str: char str
+ * Return: 0
+ */
+char *_strdup(char *str)
+{
+	char *new;
+	int a, l = 0;
+
+	a = 0;
+	while (str[a] != '\0')
+		a++;
+
+	new = malloc(sizeof(char) * (a + 1));
+
+	if (new == NULL || str == NULL)
+		return (NULL);
+
+	for (l = 0; str[l]; l++)
+		new[l] = str[l];
+
+	return (new);
+}
