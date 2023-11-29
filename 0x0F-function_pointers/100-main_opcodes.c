4d7fa503419b82022e28adc5528e4a7140bd34ac
joyeuxclement87 <tuyishimirejoyeuxclement@gmail.com>
2023-11-29 08:32:04 -0800

    working on additional qns

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entry points
+ * @argc: num of args
+ * @argv: array of args
+ * Return: o the return
+ */
+int main(int argc, char *argv[])
+{
+	int bytes, a;
+	char *array;
+
+	if (bytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	array = (char *)main;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	bytes = atoi(argv[1]);
+
+	a = 0;
+	while (a < bytes)
+	{
+		if (a == bytes - 1)
+		{
+			printf("%02hhx\n", array[a]);
+			break;
+		}
+		printf("%02hhx ", array[a]);
+		a++;
+	}
+	return (0);
+}
