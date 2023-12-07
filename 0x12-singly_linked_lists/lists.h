1222f8e5bc15693aa936b21b21e85e59a9b333a9
joyeuxclement87 <tuyishimirejoyeuxclement@gmail.com>
2023-12-07 09:56:43 -0800

    working on list.

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,12 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef LISTS_H
+#define LISTS_H
+
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
 
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
