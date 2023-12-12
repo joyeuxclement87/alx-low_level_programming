f806384fa4a22527a38513a8705ba4c9d899ce25
joyeuxclement87 <tuyishimirejoyeuxclement@gmail.com>
2023-12-12 03:16:09 -0800

    working on free listint

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * free_listint2 - a linked lists to free
+ * @head: pointer
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	if (head == NULL)
+		return;
+	while (*head)
+	{
+		temp = (*head)->next;
+		free(*head);
+		*head = temp;
+	}
+	*head = NULL;
+}
