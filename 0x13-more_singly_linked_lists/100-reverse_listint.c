647403f0cedb5082434008f904b1e10890982020
joyeuxclement87 <tuyishimirejoyeuxclement@gmail.com>
2023-12-12 07:44:41 -0800

    working on revering listint

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * reverse_listint - to reverse a stingly lists
+ * @head: pointer
+ * Return: first node to pointer
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *previous = NULL;
+	listint_t *next = NULL;
+
+	while (*head)
+	{
+		next = (*head)->next;
+		(*head)->next = previous;
+		previous = *head;
+		*head = next;
+	}
+	*head = previous;
+	return (*head);
+}
